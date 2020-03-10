3994a5dcb0b05b6cb76b468303611db60fd2e767
front-boombox <glubokij85@gmail.com>
2020-03-10 23:10:42 +0200

    added first request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Todo {
+  completed: boolean;
+  title: string;
+  id?: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
 
-  ngOnInit() {
+  public todos: Todo[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .subscribe((todos) => {
+        console.log('Todos: ', todos);
+        this.todos = todos;
+      });
   }
 }
 
